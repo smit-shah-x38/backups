f844c83c8c990ce03c266e438329b231a19eeb84
smit-shah-x38 <smit.shah@projectx38.com>
2023-06-27 11:54:32 +0000

    Mod 2

--- customtrials.py
+++ customtrials.py
@@ -12,6 +12,7 @@ import sys
 # Images, plots, display, and visualization
 import matplotlib.pyplot as plt
 import pandas as pd
+
 import seaborn as sns
 import cv2
 import IPython
@@ -20,25 +21,129 @@ from six.moves import urllib
 print(tf.__version__)
 
 # %%
-# Code for loading Data
+# # Code for loading Data
+# dataset = keras.preprocessing.image_dataset_from_directory(
+#     directory="/var/trainingData/",
+#     labels="inferred",
+#     label_mode="int",
+#     batch_size=32,
+#     image_size=(180, 180),
+#     shuffle=True,
+#     seed=123,
+# )
 
-dataset = keras.utils.image_dataset_from_directory(
+# %%
+# Assume your directory structure is:
+# main_directory/
+# ...class_a/
+# ......a_image_1.jpg
+# ......a_image_2.jpg
+# ...class_b/
+# ......b_image_1.jpg
+# ......b_image_2.jpg
+
+# Create a training dataset from the main directory with 70% of data
+train_ds = tf.keras.utils.image_dataset_from_directory(
     directory="/var/trainingData/",
     labels="inferred",
     label_mode="int",
+    image_size=(180, 180),
     batch_size=32,
     shuffle=True,
     seed=123,
+    validation_split=0.003,
+    subset="validation",
+)
+
+# Create a validation dataset from the main directory with 30% of data
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    directory="/var/trainingData/",
+    labels="inferred",
+    label_mode="int",
+    image_size=(180, 180),
+    batch_size=32,
+    shuffle=True,
+    seed=123,
+    validation_split=0.0003,
+    subset="validation",
 )
 
 # %%
-test_dataset = dataset.take(500)
-train_dataset = dataset.skip(1000)
-train_dataset = train_dataset.take(5000)
+# Get the class names from the train_ds
+class_names = train_ds.class_names
+
+# Print the class names
+print(class_names)
+
+# Get the number of labels
+num_labels = len(class_names)
+
+# Print the number of labels
+print(num_labels)
+
 
 # %%
-print(len(train_dataset))
-print(len(test_dataset))
+# Define a filter function that checks if the image has a non-zero shape
+def filter_zero_images(image, label):
+    # Get the shape of the image
+    image_shape = tf.shape(image)
+    # Check if the shape is non-zero
+    non_zero = tf.math.reduce_any(image_shape > 0)
+    # Return True or False
+    return non_zero
+
+
+# Apply the filter function to your batch dataset
+train_ds = train_ds.filter(filter_zero_images)
+
+# %%
+
+
+# %%
+# Get the number of elements in the dataset as a tensor
+num_elements = tf.data.experimental.cardinality(train_ds)
+
+# Convert the tensor to a Python integer
+num_elements = tf.get_static_value(num_elements)
+
+# Print the number of elements
+print(num_elements)
+
+
+# %%
+# Get the batch size
+batch_size = 32
+
+# Get the number of images by multiplying the number of elements by the batch size
+num_images = num_elements * batch_size
+
+# Print the number of images
+print(num_images)
+
+
+# %%
+# import torchvision.transforms as transforms
+
+# # Define a transform to convert PIL images to tensors
+# transform = transforms.ToTensor()
+
+# # Iterate through each sample in the train_dataset
+# for sample in train_dataset:
+#     # Get the image tensor from the sample
+#     image_tensor = sample[0]
+
+#     # Check if the image tensor is empty
+#     if image_tensor.numel() == 0:
+#         print("Empty image found!")
+
+#     # If you want to convert the image tensor to a PIL image for further processing:
+#     # Convert the image tensor to a PIL image
+#     image_pil = transforms.ToPILImage()(image_tensor)
+
+#     # Check if the PIL image dimensions are zero
+#     width, height = image_pil.size
+#     if width == 0 or height == 0:
+#         print("Empty image found!")
 
 
 # %%
@@ -58,33 +163,58 @@ resize_with_pad_layer = ResizeWithPadLayer(180, 180)
 
 
 # %%
-model = keras.Sequential()
-model.add(resize_with_pad_layer)
-# 32 convolution filters used each of size 3x3
-model.add(Conv2D(32, kernel_size=(3, 3), activation="relu", input_shape=(180, 180, 1)))
-# 64 convolution filters used each of size 3x3
-model.add(Conv2D(64, (3, 3), activation="relu"))
-# choose the best features via pooling
-model.add(MaxPooling2D(pool_size=(2, 2)))
-# randomly turn neurons on and off to improve convergence
-model.add(Dropout(0.25))
-# flatten since too many dimensions, we only want a classification output
-model.add(Flatten())
-# fully connected to get all relevant data
-model.add(Dense(128, activation="relu"))
-# one more dropout
-model.add(Dropout(0.5))
-# output a softmax to squash the matrix into output probabilities
-model.add(Dense(10, activation="softmax"))
+# Import the necessary modules
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
-# %%
+# Define the model architecture
+model = keras.Sequential(
+    [
+        # Rescale the pixel values to the range [0, 1]
+        layers.Rescaling(1.0 / 255, input_shape=(180, 180, 3)),
+        # Apply a convolutional layer with 16 filters and a 3x3 kernel
+        layers.Conv2D(16, 3, padding="same", activation="relu"),
+        # Apply a max pooling layer with a 2x2 window
+        layers.MaxPooling2D(),
+        # Apply another convolutional layer with 32 filters and a 3x3 kernel
+        layers.Conv2D(32, 3, padding="same", activation="relu"),
+        # Apply another max pooling layer with a 2x2 window
+        layers.MaxPooling2D(),
+        # Apply a dropout layer with a rate of 0.2 to reduce overfitting
+        layers.Dropout(0.2),
+        # Flatten the output of the previous layer
+        layers.Flatten(),
+        # Apply a dense layer with 128 units and a ReLU activation
+        layers.Dense(128, activation="relu"),
+        # Apply another dropout layer with a rate of 0.2
+        layers.Dropout(0.2),
+        # Apply an output layer with the number of classes and a softmax activation
+        layers.Dense(2002, activation="softmax"),
+    ]
+)
+
+# Compile the model with an optimizer, a loss function, and a metric
 model.compile(
-    optimizer=tf.optimizers.Adam(),
-    loss="sparse_categorical_crossentropy",
+    optimizer="adam",
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     metrics=["accuracy"],
 )
 
+# Print the model summary
+model.summary()
+
+
 # %%
-history = model.fit(train_dataset, epochs=1)
+# Train the model for 10 epochs
+model.fit(train_ds, validation_data=val_ds, epochs=10)
+
+# Evaluate the model on the test data
+loss, accuracy = model.evaluate(val_ds)
+
+# Print the loss and accuracy
+print("Loss: ", loss)
+print("Accuracy: ", accuracy)
+
 
 # %%
