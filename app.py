195ca9d22e2043a533259d932b0786c58ea1395c
smit-shah-x38 <smit.shah@projectx38.com>
2023-06-28 08:27:13 +0000

    imgtrial

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+from nbdt.utils import load_image_from_path
+from IPython.display import display
+
+from PIL import Image
+import requests
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -16,11 +21,22 @@ def helloWorld():
 @cross_origin()
 def helloImage():
     # return {"Output": "Hello, image-world!"}
-    encode_string = request.form["url"]
+    url = request.form["url_key"]
+    # url = str(url)
     # if encode_string:
     #     return {"got image": encode_string}
     # else:
     #     return {"Didn't get Image": "Empty"}
+    # im = load_image_from_path(encode_string)
+    # display(im)
+
+    # url = "https://example.com/image.jpg" # The url of the image
+    # im = load_image_from_path(url)
+    # display(im)
+    if url:
+        return {"got image": url}
+    else:
+        return {"Didn't get Image": "Empty"}
 
 
 if __name__ == "__main__":
