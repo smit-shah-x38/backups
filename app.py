072c3fb2bf2511250bca42d54d8976ee6ed50e3e
smit-shah-x38 <smit.shah@projectx38.com>
2023-06-28 07:47:50 +0000

    img route

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 
 @app.route("/test", methods=["POST"])
 @cross_origin()
 def helloWorld():
     return {"Output": "Hello, cross-origin-world!"}
+
+
+@app.route("/img", methods=["POST"])
+@cross_origin()
+def helloImage():
+    # return {"Output": "Hello, image-world!"}
+    encode_string = request.form["url"]
+    # if encode_string:
+    #     return {"got image": encode_string}
+    # else:
+    #     return {"Didn't get Image": "Empty"}
 
 
 if __name__ == "__main__":
