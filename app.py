6645f52df3ab0dc1846ae10ee25703419cc6408c
smit-shah-x38 <smit.shah@projectx38.com>
2023-06-28 07:36:55 +0000

    made Testable app

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 cors = CORS(app)
 
-@app.route("/test")
+
+@app.route("/test", methods=["POST"])
 @cross_origin()
 def helloWorld():
-  return "Hello, cross-origin-world!"
+    return {"Output": "Hello, cross-origin-world!"}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050)
