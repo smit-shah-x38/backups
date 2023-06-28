1449ca0689eb7b5aac944dc58974d6e6cb591073
smit-shah-x38 <smit.shah@projectx38.com>
2023-06-28 12:32:30 +0000

    WORKS!!!!

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask_cors import CORS, cross_origin
 from nbdt.utils import load_image_from_path
 from IPython.display import display
 from keras.models import load_model
+from keras.preprocessing import image
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
 
 from PIL import Image
 import requests
@@ -30,23 +34,30 @@ def helloWorld():
 @cross_origin()
 def helloImage():
     # return {"Output": "Hello, image-world!"}
-    url = request.json.get("url")
+    url = request.json["url"]
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    }
+    # headers = {
+    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    # }
 
-    req = urllib.request.Request(url, headers=headers)
-    response = urllib.request.urlopen(req)
-    # response = urllib.request.urlopen(url)  # get the image content
-    image = np.asarray(
-        bytearray(response.read()), dtype="uint8"
-    )  # create an array of bytes  # decode the image
+    # image = tf.keras.utils.load_img(url, target_size=(180, 180), color_mode="rgb")
 
-    image = image.resize((180, 180))  # resize the image to 224x224
-    image = image / 255.0  # normalize the pixel values
-    image = np.expand_dims(image, axis=0)  # add a batch dimension
-    pred = model.predict(image)  # get the model's prediction
+    # req = urllib.request.Request(url, headers=headers)
+    # response = urllib.request.urlopen(req)
+    # # response = urllib.request.urlopen(url)  # get the image content
+    # image = np.asarray(
+    #     bytearray(response.read()), dtype="uint8"
+    # )  # create an array of bytes  # decode the image
+
+    # # Assuming 'image' is the array you want to resize
+    # image_copy = np.copy(image)  # Make a copy of the array
+
+    # resized_image = image_copy.resize((180, 180))  # Resize the copied image
+
+    # image_copy = image_copy.resize((180, 180))  # resize the image to 224x224
+    # # image_copy = image_copy / 255.0  # normalize the pixel values
+    # # image_copy = np.expand_dims(image_copy, axis=0)  # add a batch dimension
+    # hist = model.predict(image)  # get the model's prediction
 
     # print(url)
     # url = str(url)
@@ -60,7 +71,25 @@ def helloImage():
     # url = "https://example.com/image.jpg" # The url of the image
     # im = load_image_from_path(url)
     # display(im)
-    return {"got image": str(pred)}
+
+    url = request.json["url"]
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+    with open("temp_img.jpg", "wb") as f:
+        f.write(response.content)
+
+    # Preprocess the image
+    img = load_img(
+        "temp_img.jpg", target_size=(180, 180)
+    )  # adjust the target size to match your model's expected input size
+    x = img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+
+    # Make a prediction
+    preds = model.predict(x)
+    # return str(preds)
+
+    return {"got image": str(preds)}
 
 
 if __name__ == "__main__":
