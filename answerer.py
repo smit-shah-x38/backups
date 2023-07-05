5f2d12ca8b92778d1b787bd1b081e4796e1e4229
smit-shah-x38 <smit.shah@projectx38.com>
2023-07-05 14:03:11 +0000

    Final Mod, Context + Memory

--- answerer.py
+++ answerer.py
@@ -16,20 +16,36 @@
 # ]
 
 # print(chat(messages).content)
-
+from flask import Flask, request, jsonify
 import langchain
 from langchain.llms import OpenAI
 from langchain import PromptTemplate, LLMChain
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 llm = OpenAI()
-prompt = PromptTemplate(template="Q: {question}\n A: ", input_variables=["question"])
-llm_chain = LLMChain(prompt=prompt, llm=llm)
+conversation_history = [
+    "You are a helpful assistant that specializes in fashion shopping. Only answer my questions if they are related to fashion, otherwise answer with Please ask a relevant question"
+]
 
-response = llm_chain.run("What is your name?")
-print(response)
 
-response = llm_chain.run("How old are you?")
-print(response)
+@app.route("/ask", methods=["POST"])
+@cross_origin()
+def ask():
+    global conversation_history
+    question = request.json["question"]
+    prompt = PromptTemplate(
+        template="\n".join(conversation_history) + "\nQ: {question}\n A: ",
+        input_variables=["question"],
+    )
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    response = llm_chain.run(question)
+    conversation_history.append(f"Q: {question}\nA: {response}")
+    return jsonify({"response": response})
 
-state = llm_chain.save
-print(state)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050)
