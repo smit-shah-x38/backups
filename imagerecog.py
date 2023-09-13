0bd4f53bf4c38b61946a795b8eaec086e881395e
smit-shah-x38 <smit.shah@projectx38.com>
2023-09-13 13:29:31 +0000

    Embeddings Made

---
+++ imagerecog.py
@@ -0,0 +1,37 @@
+# from deepface import DeepFace
+
+# img_path = "/var/wd_smit/localdata/Elon_Musk_Colorado_2022_(cropped2).jpg"
+# models = ["VGG-Face", "Facenet", "OpenFace", "DeepFace", "Dlib", "ArcFace"]
+# embedding = DeepFace.represent(img_path, model_name=models[0])
+
+# print(embedding[0]["facial_area"])
+
+from deepface import DeepFace
+import os
+import pandas as pd
+
+img_dir = "/var/wd_smit/internal_repos/backups/images"
+models = ["VGG-Face", "Facenet", "OpenFace", "DeepFace", "Dlib", "ArcFace"]
+embeddings = []
+face_coords = []
+
+df = pd.DataFrame({"embedding": [], "facial_area": []})
+
+for img_name in os.listdir(img_dir):
+    img_path = os.path.join(img_dir, img_name)
+    embedding = DeepFace.represent(img_path, model_name=models[0])
+    embeddings.append(embedding[0]["embedding"])
+    face_coords.append(embedding[0]["facial_area"])
+
+    dftemp = pd.DataFrame(embedding)
+    df = pd.concat([df, dftemp], ignore_index=True)
+
+print(face_coords)
+print(df.info)
+
+with open("/var/wd_smit/internal_repos/backups/embeddings_demo.txt", "w+") as f:
+    # write elements of list
+    for items in embeddings:
+        f.write("%s\n" % items)
+
+    print("File written successfully")
